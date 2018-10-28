fd6574aa29875da3f0d7ba385c0ae9333e82187f
Iceyo Kuna <iceyokuna@hotmail.com>
2018-10-29 04:44:21 +0700

    [Done] Segmentation using gradient variant methods
    using dx and dy of sobel to find gradient
    image and using standard derivatives to find threshold of background.

--- FpSegmentator.py
+++ FpSegmentator.py
@@ -20,27 +20,23 @@ class FpSegmentator:
         sd = 0
         size = self.blockSize**2
 
-##        for row in range(0,rows,self.blockSize):
-##            for col in range(0,cols,self.blockSize):
-##                try:
-##                    for r in range(row,row+16):
-##                        for c in range(col,col+16):
-##                            total += maskImg[r,c]
-##                    for r in range(row,row+16):
-##                        for c in range(col,col+16):
-##                            sd += (maskImg[r,c] - (total//size))**2
-##                    sd = math.sqrt(sd//self.blockSize)
-##                    if(total//size > 200 or total//size < 75) and sd < 150:
-##                        for r in range(row,row+16):
-##                            for c in range(col,col+16):
-##                                segmentedImg[r,c] = 0
-##                                
-##                    total = 0
-##                    sd = 0
-##                       
-##                except Exception:
-##                    pass
+
+        for row in range(0,rows,self.blockSize):
+            for col in range(0,cols,self.blockSize):
+                try:
+                    gx = cv2.Sobel(fpImg[row:row+self.blockSize, col:col+self.blockSize], cv2.CV_32F, 1, 0)
+                    gy = cv2.Sobel(fpImg[row:row+self.blockSize, col:col+self.blockSize], cv2.CV_32F, 0, 1)
+                    absGx = cv2.convertScaleAbs(gx)
+                    absGy = cv2.convertScaleAbs(gy)
+                    meanx,stdx = cv2.meanStdDev(absGx)
+                    meany,stdy = cv2.meanStdDev(absGy)
+                    if(stdx+stdy < 150):
+                        for r in range(row,row+self.blockSize):
+                            for c in range(col,col+self.blockSize):
+                                segmentedImg[r,c] = 0
                     
+                except Exception:
+                    pass     
         
         return segmentedImg, maskImg
 
@@ -48,10 +44,13 @@ class FpSegmentator:
 #-----------------------------
 
 if __name__ == "__main__":
-    img = cv2.imread("Images/1_1.bmp", cv2.IMREAD_GRAYSCALE)
+    
+    img = cv2.imread("img/1_1.bmp", cv2.IMREAD_GRAYSCALE)
     segmentator = FpSegmentator(16)
     segmentedImg,maskImg = segmentator.segment(img)
+    cv2.imshow("maskImg", maskImg)
     cv2.imshow("segment", segmentedImg)
+    
     cv2.waitKey()
     cv2.destroyAllWindows()
 
