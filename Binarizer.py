915877b69b14b0712e1b36c500fe81a89cc70d4f
Iceyo Kuna <iceyokuna@hotmail.com>
2018-10-29 23:20:59 +0700

    binarize image using adaptive thresholding

--- Binarizer.py
+++ Binarizer.py
@@ -3,19 +3,24 @@ import numpy as np
 
 #-----------------------------
 class Binarizer:
-    def binarize(self, fpImg):
+    def binarize(fpImg):
         print("Stub - Binarization")                            #stub
         print("   Input - a fingerprint image (gray-scale)")    #stub
-        print("   Output - a binary image")                     #stub
-        binImg = fpImg                                          #stub
+        print("   Output - a binary image")                     #stub                                      #stub
+
+        binImg = cv2.adaptiveThreshold(img, 255,adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, \
+                                                thresholdType=cv2.THRESH_BINARY, blockSize=9, C=10)
+        
         return binImg
         
 #-----------------------------
 if __name__ == "__main__":
-    img = cv2.imread("1_1.bmp", cv2.IMREAD_GRAYSCALE)
-    binImg = Binarizer.binarize(img)
-    cv2.imshow("binary", binImg)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    pass
+##    img = cv2.imread("img/1_1.bmp", cv2.IMREAD_GRAYSCALE)
+##    cv2.imshow("img", img)
+##    binImg = Binarizer.binarize(img)
+##    cv2.imshow("binary", binImg)
+##    cv2.waitKey()
+##    cv2.destroyAllWindows()
 
 #-----------------------------
--- FpSegmentator.py
+++ FpSegmentator.py
@@ -25,7 +25,8 @@ class FpSegmentator:
             for col in range(0,cols,self.blockSize):
                 try:
                     gx = cv2.Sobel(fpImg[row:row+self.blockSize, col:col+self.blockSize], cv2.CV_32F, 1, 0)
-                    gy = cv2.Sobel(fpImg[row:row+self.blockSize, col:col+self.blockSize], cv2.CV_32F, 0, 1)
+                    gy = cv2.Sobel(fpImg[row:row+self.blockSize, col:col+self.blockSize], cv2.CV_32F, 0, 1)
+
                     absGx = cv2.convertScaleAbs(gx)
                     absGy = cv2.convertScaleAbs(gy)
                     meanx,stdx = cv2.meanStdDev(absGx)
@@ -33,7 +34,7 @@ class FpSegmentator:
                     if(stdx+stdy < 150):
                         for r in range(row,row+self.blockSize):
                             for c in range(col,col+self.blockSize):
-                                segmentedImg[r,c] = 0
+                                segmentedImg[r,c] = 255
                     
                 except Exception:
                     pass     
@@ -44,14 +45,14 @@ class FpSegmentator:
 #-----------------------------
 
 if __name__ == "__main__":
-    
-    img = cv2.imread("img/1_1.bmp", cv2.IMREAD_GRAYSCALE)
-    segmentator = FpSegmentator(16)
-    segmentedImg,maskImg = segmentator.segment(img)
-    cv2.imshow("maskImg", maskImg)
-    cv2.imshow("segment", segmentedImg)
-    
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    pass
+##    img = cv2.imread("img/1_1.bmp", cv2.IMREAD_GRAYSCALE)
+##    cv2.imshow("img", img)
+##    segmentator = FpSegmentator(16)
+##    segmentedImg,maskImg = segmentator.segment(img)
+##    cv2.imshow("segment", segmentedImg)
+##    
+##    cv2.waitKey()
+##    cv2.destroyAllWindows()
 
 #-----------------------------
