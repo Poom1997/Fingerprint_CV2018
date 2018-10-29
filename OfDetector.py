19a5473611bca66f6fba7e83373e92157b6771d7
Iceyo Kuna <iceyokuna@hotmail.com>
2018-10-29 21:21:41 +0700

    implemeting Ofdetector

--- OfDetector.py
+++ OfDetector.py
@@ -9,18 +9,24 @@ class OfDetector:
     def detect(self, fpImg, mskImg):
         
         height, width = fpImg.shape
+
+        gx = cv2.Sobel(fpImg, cv2.CV_32F, 1, 0)
+        gy = cv2.Sobel(fpImg, cv2.CV_32F, 0, 1)
+
+        for row in range(0,width):
+            for col in range(0,height):
+                fpImg[row,col] = 0
+                
         
-        ofMat = fpImg
-        ofImg = fpImg
-        return ofMat, ofImg
+        return fpImg, fpImg
         
 #-----------------------------
 if __name__ == "__main__":
     inpImg = cv2.imread("img/1_1.BMP", cv2.IMREAD_GRAYSCALE)
     ofDetect = OfDetector()
-    outMat, outImg = ofDetect.detect(inpImg, inpImg)
-    cv2.imshow("input", inpImg)
-    cv2.imshow("Output", inpImg)
+    absGx, absGy = ofDetect.detect(inpImg, inpImg)
+    cv2.imshow("absGx", absGx)
+    cv2.imshow("absGy", absGy)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
