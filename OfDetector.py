42596dc6f223cb1aa67355120378c26974c71693
Bhurinuth Wongsrisakul <poom1997@gmail.com>
2018-10-30 16:03:31 +0700

    Finish OFDetector

--- OfDetector.py
+++ OfDetector.py
@@ -8,34 +8,40 @@ class OfDetector:
         pass
     
     def detect(self, fpImg):
-        
+        #Detect Dimensions
         height, width = fpImg.shape
 
+        #Calculate Gradient
         gx = cv2.Sobel(fpImg, cv2.CV_32F, 1, 0)
         gy = cv2.Sobel(fpImg, cv2.CV_32F, 0, 1)
 
+        #Var. Init.
         orientation = 0
         sum1 = 0
-        sum2 = 0
-        
-        for row in range(0,width):
-            for col in range(0,height):
-                sum1 += 2 * gx[row,col] * gy[row,col]
-                sum2 += (gx[row,col] ** 2) - (gy[row,col] ** 2)
+        sum2 = 1
+        of = np.zeros((16,16))
 
-        orientation = 0.5 * math.atan(sum1 / sum2)
-        orientation = math.degrees(orientation)
-        
-        return orientation
+        for row in range(0,width, 16):
+            for col in range(0,height, 16):
+                tmp = []
+                for i in range(row, row + 16):
+                    for j in range(col, col + 16): 
+                        sum1 += 2 * gx[i,j] * gy[i,j]
+                        sum2 += (gx[i,j] ** 2) - (gy[i,j] ** 2)
+                orientation = 0.5 * math.atan(sum1 / sum2)     
+                orientation = math.degrees(orientation)
+                of[row//16,col//16] = orientation+90
+
+        return of
         
 #-----------------------------
 if __name__ == "__main__":
     pass
 ##    inpImg = cv2.imread("img/1_1.BMP", cv2.IMREAD_GRAYSCALE)
 ##    ofDetect = OfDetector()
-##    absGx, absGy = ofDetect.detect(inpImg, inpImg)
-##    cv2.imshow("absGx", absGx)
-##    cv2.imshow("absGy", absGy)
+##    temp = ofDetect.detect(inpImg)
+##    height, width = inpImg.shape
+##    print(temp)
 ##    cv2.waitKey()
 ##    cv2.destroyAllWindows()
 
